8768e60adafb7699854acef6256f34313d1c93da
Somanath Barge <somanath@nimap.com>
2023-06-09 17:56:44 +0530

    sec que

---
+++ ReverseNumber/ReverseNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 12345;
+            int rev = 0;
+            int temp = 0;
+            while(num > 0)
+            {
+                temp = num % 10;
+                rev=(rev*10)+temp;
+                num /= 10;
+            }
+            Console.WriteLine(rev);
+            Console.ReadLine();
+        }
+
+    }
+}
