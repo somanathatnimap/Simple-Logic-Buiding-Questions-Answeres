4b35bc1f518b5293ba116ce433f3e54df21e369f
Somanath Barge <somanath@nimap.com>
2023-09-02 18:49:24 +0530

    saturday

---
+++ Logicals/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logicals
+{
+    internal class RemoveElementInArray
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5,5 };
+            int number = 5;
+            /*
+            arr=arr.Where(x=>x!= number).ToArray();
+            for(int i=0;i<arr.Length; i++)
+            {
+                Console.WriteLine($"Element in array are {arr[i]}");
+            }
+                Console.WriteLine($"Length of array is {arr.Length}");
+            */
+            Array.FindAll(arr, x=>x!=5);
+            List<int> list = new List<int>()
+           {
+               1,3,4,5,6,7
+           };
+            list.RemoveAt(1);
+            Console.ReadLine();
+
+            
+        }
+    }
+}
---
+++ Logicals/removeDublicate.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logicals
+{
+    internal class removeDublicate
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1,1, 2, 3, 4, 5, 6, 7,3,2};
+            int a=Array.IndexOf(arr, 2);
+            for(int i=0;i<arr.Length; i++)
+            {
+                int num = 0;
+                for(int j = i + 1; j < arr.Length; j++)
+                {
+                    if(arr[j] == arr[i])
+                    {
+                        num = arr[j];
+                        break;
+                    }
+                }
+                if(num != 0)
+                {
+            int[] arr2=arr.Where(x => x == num).ToArray();
+                Console.WriteLine(num);
+            for(int j=0;j< arr2.Length; j++) 
+                    {
+                        Console.WriteLine("ar2"+arr[j]);
+                    }
+
+                }
+        }
+                Console.ReadLine();
+            }
+    }
+}
+
+/*
+ 
+ int[] arr = { 1,1, 2, 3, 4, 5, 6, 7,3,2};
+            int count = 0;
+       //     int[] arr2= new int[count];
+            for(int i=0;i<arr.Length; i++)
+            {
+                int a = 0;
+                for(int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        a = arr[i];
+                    }
+                if(a != 0)
+                {
+                count++;
+                Console.WriteLine(a+""+count);
+                break;
+                }
+                }
+                int[] arr2=new int[count];
+                for(int j=0;j<arr2.Length ; j++)
+                {
+                    arr2[j] = a;
+                    Console.WriteLine(+arr[j]);
+                }
+               
+            }
+                Console.WriteLine("count"+count);
+                Console.ReadLine();
+        }
+ 
+ */
