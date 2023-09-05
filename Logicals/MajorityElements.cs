8a87325a6a28a60d1b42282ce5359dc1215439aa
Somanath Barge <somanath@nimap.com>
2023-09-05 18:18:20 +0530

    5/9/23

---
+++ Logicals/MajorityElements.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logicals
+{
+    internal class MajorityElements
+    {
+        public int MajorityElement(int[] nums)
+        {
+            int candidate = 0;
+            int count = 0;
+
+            for(int i=0;i<nums.Length;i++)
+            {
+                if (count == 0)
+                {
+                    candidate = nums[i];
+                }
+
+                if (nums[i] == candidate)
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                }
+            }
+
+            return candidate;
+        }
+        static void Main(string[] args)
+        {
+        MajorityElements majorityElements = new MajorityElements();
+        int[] nums1 = { 3, 2, 3 };
+        int[] nums2 = { 2, 2, 1, 1, 1, 2, 2 };
+
+            int result1= majorityElements.MajorityElement(nums1);
+            int result2 = majorityElements.MajorityElement(nums2);
+
+        Console.WriteLine(result1); 
+        Console.WriteLine(result2); 
+        Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ Logicals/SumOfGivenRange.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logicals
+{
+    internal class SumOfGivenRange
+    {
+        public int Sum(int start, int end)
+        {
+            int count = start;
+            int sum = 0;
+            while (count <= end)
+            {
+                sum=sum+ count;
+                count++;
+            }
+            return sum;
+        }
+        public static void Main(string[] args)
+        {
+            SumOfGivenRange sumOfGivenRange = new SumOfGivenRange();
+            int x=sumOfGivenRange.Sum(10, 20);
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
